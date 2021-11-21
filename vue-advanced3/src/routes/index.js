8018187bd86ae4bf7e204944353cfa821ce63061
yoon <yoonemong@naver.com>
2021-11-21 14:37:36 +0900

    axios

--- vue-advanced3/src/routes/index.js
+++ vue-advanced3/src/routes/index.js
@@ -3,10 +3,13 @@ import VueRouter from "vue-router";
 import NewsView from '../views/NewsView.vue';
 import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
 
 Vue.use(VueRouter);
 
 export const router = new VueRouter({
+    mode: 'history',
     routes: [
         {
             path: "/",
@@ -29,6 +32,14 @@ export const router = new VueRouter({
             path: '/jobs',
             // component: url 주소로 갔을 대 표시될 컴포넌트
             component: JobsView,
+        },
+        {
+            path: '/user',
+            component: UserView,
+        },
+        {
+            path: '/item',
+            component: ItemView
         }
     ]
 })
