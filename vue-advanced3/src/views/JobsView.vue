d97027c8b8aa3762028c561261977344b3ff8b7a
yoon <yoonemong@drmamma.co.kr>
2021-11-30 23:46:24 +0900

    vuex

--- vue-advanced3/src/views/JobsView.vue
+++ vue-advanced3/src/views/JobsView.vue
@@ -1,13 +1,12 @@
 <template>
   <div>
-    <div v-for="job in jobs">
+    <div v-for="job in this.$store.state.jobs">
       {{job}}
     </div>
   </div>
 </template>
 
 <script>
-import {fetchJobsList} from '../api/index.js'
 
 export default {
   data(){
@@ -16,9 +15,7 @@ export default {
     }
   },
   created(){
-    fetchJobsList()
-    .then(response => this.jobs = response.data)
-    .catch(error => console.log(error))
+    this.$store.dispatch('FETCH_JOBS')
   }
 }
 </script>
