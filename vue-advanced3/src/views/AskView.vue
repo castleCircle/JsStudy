458dfecd0a9324b5eeeb6a5f3c6f2549c2f0fa3f
yoon <yoonemong@naver.com>
2021-12-01 23:58:13 +0900

    mapGetter

--- vue-advanced3/src/views/AskView.vue
+++ vue-advanced3/src/views/AskView.vue
@@ -1,12 +1,21 @@
 <template>
   <div>
-    <div v-for="item in this.$store.state.ask">{{ item }}</div>
+    <div v-for="item in fetchedAsk">{{ item }}</div>
   </div>
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'; 
 
 export default {
+  computed: {
+    // ...mapState({
+    //   ask: state => state.ask
+    // })
+    ...mapGetters({
+      fetchedAsk: 'fetchedAsk'
+    })
+  },
   created() {
     this.$store.dispatch('FETCH_ASK')
   },
