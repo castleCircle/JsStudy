c83345098434a983574639a453158efaed7c93f0
yoon <yoonemong@drmamma.co.kr>
2021-12-02 23:36:00 +0900

    store

--- vue-advanced3/src/store/index.js
+++ vue-advanced3/src/store/index.js
@@ -1,7 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import { fetchNewsList , fetchJobsList ,fetchAskList } from '../api/index.js'
+import mutations from './mutations.js'
+import actions from './action'
 
 Vue.use(Vuex);
 
@@ -16,44 +17,6 @@ export const store = new Vuex.Store({
             return state.ask;
         }
     },
-    mutations: {
-        SET_NEWS(state,news){
-            state.news = news;
-        },
-        SET_JOBS(state,jobs){
-            state.jobs = jobs;
-        },
-        SET_ASK(state,ask){
-            state.ask = ask;
-        }
-    },
-    actions:{
-        FETCH_NEWS(context) {
-            fetchNewsList()
-            .then(response=>{
-                context.commit('SET_NEWS',response.data);
-            })
-            .catch(error=>{
-                console.log(error);
-            })
-        },
-        FETCH_JOBS({commit}){
-            fetchJobsList()
-            .then(({data})=>{
-                commit('SET_JOBS',data);
-            })
-            .catch(error=>{
-                console.log(error)
-            })
-        },
-        FETCH_ASK({commit}){
-            fetchAskList()
-            .then(({data})=>{
-                commit("SET_ASK",data)
-            })
-            .catch(error=>{
-                console.log(error);
-            });
-        }
-    }
+    mutations,
+    actions
 });
