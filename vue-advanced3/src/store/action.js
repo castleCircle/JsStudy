19ff946d97d859cf5f1dcf9bf5838d3ffd5e749c
yoon <yoonemong@naver.com>
2021-12-06 20:47:28 +0900

    user

--- vue-advanced3/src/store/action.js
+++ vue-advanced3/src/store/action.js
@@ -1,4 +1,4 @@
-import { fetchNewsList , fetchJobsList ,fetchAskList } from '../api/index.js'
+import { fetchNewsList , fetchJobsList ,fetchAskList ,fetchUserInfo} from '../api/index.js'
 
 export default {
     FETCH_NEWS(context) {
@@ -26,6 +26,15 @@ export default {
         })
         .catch(error=>{
             console.log(error);
+        });
+    },
+    FETCH_USER({commit},name){
+        fetchUserInfo(name)
+        .then(({data})=>{
+            commit('SET_USER',data);
+        })
+        .catch(error => {
+            console.log(error)
         });
     }
 }
