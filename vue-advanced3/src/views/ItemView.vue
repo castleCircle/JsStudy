e67db1c08cb7297ac5189be96aa795cd4c028bfa
yoon <yoonemong@naver.com>
2021-12-10 22:03:12 +0900

    v-html

--- vue-advanced3/src/views/ItemView.vue
+++ vue-advanced3/src/views/ItemView.vue
@@ -1,21 +1,37 @@
 <template>
   <div>
-      <p>
-          test: {{itemInfo.title}}
-      </p>
-      <div>
-          test: {{itemInfo.content}}
-      </div>
+      <section>
+          <!-- 질문상세 -->
+          <div class="user-container">
+            <div>
+                <i class="fas fa-user"></i>
+            </div>
+            <div class="user-description">
+                <router-link :to="`/user/${fetchedItem.user}`">
+                    {{fetchedItem.user}}
+                </router-link>
+                <div>
+                    {{fetchedItem.time_ago}}
+                </div>
+            </div>
+          </div>
+          <h2>{{fetchedItem.title}}</h2>
+      </section>
+      <section>
+          <!-- 질문 댓글 -->
+          <div v-html="fetchedItem.content">
+          </div>
+      </section>
   </div>
 </template>
 
 <script>
 
+import {mapGetters} from 'vuex'
+
 export default {
     computed: {
-        itemInfo(){
-            return this.$store.state.item
-        }
+        ...mapGetters(['fetchedItem']),
     },
     created(){
         const itemId = this.$route.params.id;
@@ -24,6 +40,12 @@ export default {
 }
 </script>
 
-<style>
-
+<style scoped>
+.user-container {
+    display: flex;
+    align-items: center;
+}
+.fa-user{
+    font-size: 2.5rem;
+}
 </style>
