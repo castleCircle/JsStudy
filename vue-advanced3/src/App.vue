a16d1636c289ee766f42bdfd6439909f2c218be1
yoon <yoonemong@naver.com>
2021-12-16 23:13:58 +0900

    transition

--- vue-advanced3/src/App.vue
+++ vue-advanced3/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -15,4 +17,10 @@ export default {
 </script>
 
 <style>
+.page-enter-active, .page-leave-active {
+  transition: opacity .5s;
+}
+.page-enter, .page-leave-to /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
 </style>
