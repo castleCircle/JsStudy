72cf68a4d2098e492a48a344aa3b19a1e9c92dab
yoon <yoonemong@naver.com>
2022-01-09 22:11:05 +0900

    HOC

--- vue-advanced3/src/routes/index.js
+++ vue-advanced3/src/routes/index.js
@@ -5,6 +5,7 @@ import AskView from '../views/AskView.vue';
 import JobsView from '../views/JobsView.vue';
 import UserView from '../views/UserView.vue';
 import ItemView from '../views/ItemView.vue';
+import createListView from '../view/CreateListView';
 
 Vue.use(VueRouter);
 
@@ -20,21 +21,24 @@ export const router = new VueRouter({
             path: '/news',
             // component: url 주소로 갔을 대 표시될 컴포넌트
             name: 'news',
-            component: NewsView,
+            // component: NewsView,
+            component: createListView('NewsView'),
         },
 		{
             //url 주소
             path: '/ask',
             // component: url 주소로 갔을 대 표시될 컴포넌트
             name: 'ask',
-            component: AskView,
+            // component: AskView,
+            component: createListView('AskView'),
         },
 		{
             //url 주소
             path: '/jobs',
             // component: url 주소로 갔을 대 표시될 컴포넌트
             name: 'jobs',
-            component: JobsView,
+            // component: JobsView,
+            component: createListView('JobsView'),
         },
         {
             path: '/user/:id',
